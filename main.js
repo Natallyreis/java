fcd74c0c892c4fd4bf1e7fa9df8fb6ad13738e23
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-08-17 09:08:30 -0300

    commit

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-//declaração de variaveis 
-let operação = prompt("Informe a operação desejada: ")
+let operacao = prompt("Inorme a operação desejada: ")
 let numero1 = Number(prompt("Informe o primeiro número: "))
 let numero2 = Number(prompt("Informe o segundo número: "))
 
-//verificar a operação matemática selecionada
-if (operação == "+") {
+if (operacao == "+"){
     let resultado = numero1 + numero2
-    // prompt ("O resultado da soma é " + resultado + ".")
-    document.write('O resultado da soma é ' + resultado + '!');
+    document.write("O resultado da soma é " + resultado + ".")
 }
-if (operação == "-") {
+else if (operacao == "-"){
     let resultado = numero1 - numero2
-    // console.log ("O resultado da subtração é " + resultado + ".")
-    document.write('O resultado da subtração é ' + resultado + '!');
-}if (operação == "*") {
+    document.write("O resultado da subtração é " + resultado + ".")
+}
+else if (operacao == "*"){
     let resultado = numero1 * numero2
-    // console.log ("O resultado da multiplicação é " + resultado + ".")
-    document.write('O resultado da multiplicação é ' + resultado + '!');
-}if (operação == "/") {
+    document.write("O resultado da multiplicação é " + resultado + ".")
+}
+else if (operacao == "/"){
     let resultado = numero1 / numero2
-    // console.log ("O resultado da divisão é " + resultado + ".")
-    document.write('O resultado da divisão é ' + resultado + '!');
+    document.write("O resultado da divisão é " + resultado + ".")
+} else{
+    document.write("OPERAÇÃO INCORRETA")
 }
 
