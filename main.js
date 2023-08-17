42e5c5b06fa46f57a23e1690d9d2dcc0617a7729
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-08-17 09:11:38 -0300

    commit

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-let operação = prompt("Informe a operação desejada:")
-let numero1 = Number(prompt("Informe o primeiro numero:"))
-let numero2 = Number(prompt("Informe o segundo numero:"))
+let operacao = prompt("Inorme a operação desejada: ")
+let numero1 = Number(prompt("Informe o primeiro número: "))
+let numero2 = Number(prompt("Informe o segundo número: "))
 
-if(operação == "+") {
-     let resultado = numero1 + numero2
-     document.write("O resultado da soma é " + resultado + ".")
-
+if (operacao == "+"){
+    let resultado = numero1 + numero2
+    document.write("O resultado da soma é " + resultado + ".")
 }
-
-else if (operação == "-") {
+else if (operacao == "-"){
     let resultado = numero1 - numero2
-    document.write ("O resultado da subtraçaõ é " + resultado + '.')
+    document.write("O resultado da subtração é " + resultado + ".")
+}
+else if (operacao == "*"){
+    let resultado = numero1 * numero2
+    document.write("O resultado da multiplicação é " + resultado + ".")
+}
+else if (operacao == "/"){
+    let resultado = numero1 / numero2
+    document.write("O resultado da divisão é " + resultado + ".")
+} else{
+    document.write("OPERAÇÃO INCORRETA")
 }
 
-else if (operação == '*') {
-    let resultado = numero1 * numero2
-    document.write ("O resultado da multiplicação é" + resultado + ".")
-}
-   
-else if (operação == "/") {
-    let resultado = numero1 / numero2 
-    document.write ("O resultado da divisão é" + resultado + ".")
-}
